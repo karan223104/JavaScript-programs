a3c26f05f48ac2c8d0e4ccf2f129e827da3beefd
Karan Chauhan <kc8140898189@gmail.com>
2026-02-03 17:45:18 +0530

    first commit

---
+++ variale.js
@@ -0,0 +1,12 @@
+console.log("karan chauhan");
+
+fullName = "karan";
+age = 32;
+price = 99;
+radius = 3.9;
+a = null;
+y = undefined;
+isMarried = false;
+
+console.log(fullName);
+console.log(age);
