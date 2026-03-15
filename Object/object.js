a95772d2c0d6be76012674934c98b7f07a9e96e6
Karan Chauhan <kc8140898189@gmail.com>
2026-03-15 23:00:40 +0530

    object,object literal,switchcase,scope,higherorder function

---
+++ Object/object.js
@@ -0,0 +1,12 @@
+let car = {
+  brand: "Tesla",
+  model: "Model S",                            // {} object literal
+  year: 2022
+};
+
+// Accessing properties using dot notation
+console.log(car.brand);  
+console.log(car.model);  
+
+// Accessing properties using bracet notation
+console.log(car["year"]);
---
+++ SwitchCase/switchcase.js
@@ -0,0 +1,19 @@
+let color = "red";
+
+switch (color) {
+    case "red":
+        console.log("stop");
+        break;
+
+    case "yellow":
+        console.log("slow down");
+        break;
+
+    case "green":
+        console.log("go");
+        break;
+
+    default:
+        console.log("light is broken");
+}
+
